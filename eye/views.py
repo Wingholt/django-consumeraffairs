2713587743a045f83aebe480f9aa079811f1140f
Wing <wing.software.engineer@gmail.com>
2021-10-30 17:23:57 +0100

    Add future timestamp check

--- eye/views.py
+++ eye/views.py
@@ -8,6 +8,48 @@ from eye import validator
 
 alldata = []  # all users' input in cache
 
+
+alldata = [
+{
+  "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+  "category": "page interaction",
+  "name": "pageview",
+  "data": {
+    "host": "www.consumeraffairs.com",
+    "path": "/"
+  },
+  "timestamp": "2021-01-01 07:15:27.243860"
+}
+,
+{
+  "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+  "category": "page interaction",
+  "name": "cta click",
+  "data": {
+    "host": "www.consumeraffairs.com",
+    "path": "/",
+    "element": "chat bubble"
+  },
+  "timestamp": "2021-01-01 03:15:27.243860"
+},
+
+{
+  "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+  "category": "form interaction",
+  "name": "submit",
+  "data": {
+    "host": "www.consumeraffairs.com",
+    "path": "/",
+    "form": {
+      "first_name": "John",
+      "last_name": "Doe"
+    }
+  },
+  "timestamp": "2021-01-01 11:15:27.243860"
+}]
+
+
+
 '''Count class to put into buckets according to key'''
 class Count():
 
@@ -75,6 +117,7 @@ class Report():
 
         try:
 
+            text = "Nothing found"
             key, value = list(input.items())[0]
     
             if (type(value) is str):    # session_id, category or name
